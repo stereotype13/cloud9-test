f334ea1422b6f32451abd639e979312280769c10
OpenShift guest <robert.hodel@ladwp.com>
2013-12-27 14:07:25 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloud9Test::Application.config.secret_token = '4c6d18fe1b89dcfcf58aaa9269e8e4a5012141f342b10c273c6e27ec0318fa58dc702684db6f0257ce2a5747e348a0995b1c08329e3a099ec3ae968a1a8a6ade'
